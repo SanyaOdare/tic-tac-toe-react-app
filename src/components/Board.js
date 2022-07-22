c140b9b031f9b4aaa5bc276fb1a055cb208caed9
SanyaOdare <sanyaodare@gmail.com>
2022-07-22 15:03:32 +0100

    test the app

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Square from './Square';
 
-const Board = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const Board = ({ squares, onClick }) => (
+  <div className='board'>
+    {squares.map((square, index) => (
+      <Square
+        key={index}
+        value={square}
+        onClick={() => onClick(index)}
+      />
+    ))}
+  </div>  
+);
 
-export default Board
+export default Board;
